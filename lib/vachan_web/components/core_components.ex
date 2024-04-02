6a9361b4537f907c1439ae97dfcff809f08e986d
bind-ritu1211 <kumariritubind@gmail.com>
2024-04-02 16:08:12 +0530

    update styles for table

--- lib/vachan_web/components/core_components.ex
+++ lib/vachan_web/components/core_components.ex
@@ -477,52 +477,6 @@ defmodule VachanWeb.CoreComponents do
       end
 
     ~H"""
-    <%!-- <div class="overflow-y-auto px-4 sm:overflow-visible sm:px-0 rounded-none p-0 bg-white border-t-0 border-collapse table-fixed shadow-inner shadow border-b border-gray-300">
-      <table class="w-[40rem] mt-5 sm:w-full">
-        <thead class="text-sm text-left leading-6 text-zinc-500">
-          <tr>
-            <th :for={col <- @col} class="p-0 pb-4 pr-6 py-3 px-12 uppercase text-xs font-semibold">
-              <%= col[:label] %>
-            </th>
-            <th :if={@action != []} class="relative p-0 pb-4 uppercase text-xs font-semibold">
-              <span class="sr-only"><%= gettext("Actions") %></span>
-            </th>
-          </tr>
-        </thead>
-        <tbody
-          id={@id}
-          phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
-          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
-        >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50 ">
-            <td
-              :for={{col, i} <- Enum.with_index(@col)}
-              phx-click={@row_click && @row_click.(row)}
-              class={["relative px-14", @row_click && "hover:cursor-pointer"]}
-            >
-              <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
-                <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
-                  <%= render_slot(col, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-            <td :if={@action != []} class="relative w-14 px-14">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
-                <span
-                  :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                >
-                  <%= render_slot(action, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div> --%>
-
     <div class="container mx-auto  pt-5 shadow">
       <table class="min-w-full divide-y divide-gray-200">
         <thead>
@@ -556,12 +510,7 @@ defmodule VachanWeb.CoreComponents do
                 <%= render_slot(col, @row_item.(row)) %>
               </span>
             </td>
-
-            <td
-              :if={@action != []}
-              phx-click={@row_click && @row_click.(row)}
-              class={["relative px-8 py-2", @row_click && "hover:cursor-pointer"]}
-            >
+            <td :if={@action != []}>
               <span
                 :for={action <- @action}
                 class="relative ml-4 font-bold text-sm text-zinc-900 hover:text-zinc-700"
