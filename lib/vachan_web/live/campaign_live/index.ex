8c932814c7f730a699327a8a7884de2b4c657c73
bind-ritu1211 <kumariritubind@gmail.com>
2024-05-27 11:53:45 +0530

    patch: remove paginnation from campaigns

--- lib/vachan_web/live/campaign_live/index.ex
+++ lib/vachan_web/live/campaign_live/index.ex
@@ -6,30 +6,8 @@ defmodule VachanWeb.CampaignLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok,
-     socket
-     |> assign(:pages, 0)
-     |> assign(:active_page, 1)
-     |> assign(:page_offset, 0)
-     |> assign(:page_limit, 50)
-     |> stream(:campaigns, [])}
-  end
-
-  @impl true
-  def handle_params(params, _url, socket) do
-    socket =
-      socket
-      |> maybe_assign(:active_page, page(params["page"]))
-      |> maybe_assign(:page_offset, page_offset(page(params["page"]), socket.assigns.page_limit))
-
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :index, _params) do
-    page = list_campaigns(socket)
-
-    socket
-    |> stream(:campaigns, page, reset: true)
+    {:ok, campaigns} = Campaign.read_all(ash_opts(socket))
+    {:ok, stream(socket, :campaigns, campaigns)}
   end
 
   def handle_event("run", %{"id" => id}, socket) do
@@ -48,35 +26,11 @@ defmodule VachanWeb.CampaignLive.Index do
     {:noreply, stream(socket, :campaigns, campaigns, reset: true)}
   end
 
-  defp list_campaigns(socket) do
-    Campaign.read_all!(ash_opts(socket))
-  end
-
   defp search_campaign_name(query, socket) when is_binary(query) do
     {:ok, campaigns} = Campaign.read_all(ash_opts(socket))
     capitalized_query = String.capitalize(query)
-
     Enum.filter(campaigns, fn campaign ->
       String.contains?(String.capitalize(campaign.name), capitalized_query)
     end)
   end
-
-  defp page(nil), do: nil
-
-  defp page(page_param) do
-    {active_page, _} = Integer.parse(page_param)
-    active_page
-  end
-
-  defp page_offset(nil, _page_limit), do: nil
-
-  defp page_offset(page_param, page_limit) do
-    (page_param - 1) * page_limit
-  end
-
-  defp maybe_assign(socket, _key, nil), do: socket
-  defp maybe_assign(socket, key, val), do: socket |> assign(key, val)
-
-  def active_class(on_page, active_page) when on_page == active_page, do: "active"
-  def active_class(_on_page, _active_page), do: ""
 end
