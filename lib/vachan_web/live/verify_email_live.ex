2e3963a5fcf0cfe5f7e8686d74da2ac73f7b4d73
Yuvraj Rathi <yrathi66@gmail.com>
2024-06-10 10:26:15 +0530

    Verify your email page updated

--- lib/vachan_web/live/verify_email_live.ex
+++ lib/vachan_web/live/verify_email_live.ex
@@ -7,9 +7,16 @@ defmodule VachanWeb.VerifyEmailLive do
 
   def render(assigns) do
     ~H"""
-    <div class="flex justify-center mt-12">
-      <h1>Verify Email</h1>
-      You must verify your email.
+    <div class="flex justify-center items-center min-h-screen" style="background-color: rgb(237, 237, 208);">
+      <div class="bg-white p-8 rounded shadow-md text-center max-w-md w-full">
+        <h1 class="text-3xl font-bold text-red-600 mb-4">Verify Your Email</h1>
+        <p class="text-lg">
+          You must verify your email before proceeding.
+        </p>
+        <p class="text-lg mt-4">
+          Please check the inbox of the email account you just registered with for the verification link.
+        </p>
+      </div>
     </div>
     """
   end
