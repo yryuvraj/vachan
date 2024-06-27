c1fe18c5b0c58b8c2450e69ff77a1e145b4edcbb
Yuvraj Rathi <86550457+yryuvraj@users.noreply.github.com>
2024-06-27 11:43:42 +0530

    Fix tooltip targeting for settings and logout buttons

--- lib/vachan_web/live/common/sidebar_menu.ex
+++ lib/vachan_web/live/common/sidebar_menu.ex
@@ -139,11 +139,19 @@ defmodule VachanWeb.SidebarMenuLiveComponent do
         </div>
         <a
           href="/sign-out"
-          data-tooltip-target="tooltip-settings"
+          data-tooltip-target="tooltip-logout"
           class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-customBackground_header dark:hover:bg-gray-600"
         >
           Logout
         </a>
+        <div
+          id="tooltip-logout"
+          role="tooltip"
+          class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+        >
+          Logout
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
       </div>
     </div>
     """
